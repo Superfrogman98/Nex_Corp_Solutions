ae85355209854913f889382e0688c5033c4a7750
Superfrogman98 <frogman98m@aol.com>
2017-05-27 00:53:04 -0500

    Worktable now successfully uses TESR to render the first item on top, will continue experimenting with. Will also update workbench to have a specific front side that the item will be rendered away from.

--- src/main/java/Superfrogman98/NCS/NexCorpSolutions.java
+++ src/main/java/Superfrogman98/NCS/NexCorpSolutions.java
@@ -11,6 +11,8 @@ import Superfrogman98.NCS.Blocks.ModBlocksCrops;
 import Superfrogman98.NCS.Client.NCS_Tab;
 import Superfrogman98.NCS.Items.ModItems;
 import Superfrogman98.NCS.Items.Tools.CustomToolClasses;
+import Superfrogman98.NCS.Network.PacketRequestUpdateBasicWorktable;
+import Superfrogman98.NCS.Network.PacketUpdateBasicWorktable;
 import Superfrogman98.NCS.Recipes.ModRecipes;
 import Superfrogman98.NCS.World.ModWorldGen;
 import net.minecraftforge.fml.common.Mod;
@@ -18,7 +20,10 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = NexCorpSolutions.modId, name = NexCorpSolutions.name, version = NexCorpSolutions.version, acceptedMinecraftVersions = "[1.10.2]")
 
@@ -28,7 +33,7 @@ public class NexCorpSolutions {
     public static final String name = "Nex Corp Solutions";
     public static final String version = "0.0.1";
     public static final NCS_Tab creativeTab = new NCS_Tab();
-
+    public static SimpleNetworkWrapper network;
 
     //initialize proxy
     @SidedProxy(serverSide = "Superfrogman98.NCS.ServerProxy", clientSide = "Superfrogman98.NCS.ClientProxy")
@@ -45,7 +50,9 @@ public class NexCorpSolutions {
         ModItems.init();
         ModBlocks.init();
         GameRegistry.registerWorldGenerator(new ModWorldGen(),3);
-
+        network = NetworkRegistry.INSTANCE.newSimpleChannel(modId);
+        network.registerMessage(new PacketUpdateBasicWorktable.Handler(), PacketUpdateBasicWorktable.class, 0 , Side.CLIENT );
+        network.registerMessage(new PacketRequestUpdateBasicWorktable.Handler(), PacketRequestUpdateBasicWorktable.class, 1, Side.SERVER);
     }
 
     @Mod.EventHandler
