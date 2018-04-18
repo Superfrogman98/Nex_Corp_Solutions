d5e98e6e3dbfddcd30e4055a360bc566102d91d7
Superfrogman98 <superfrogman98m@gmail.com>
2018-04-18 00:22:12 -0500

    Moving to 1.12.2, will be completly rewritten, cleaning up the file system down to just the main class

--- README.txt
+++
@@ -1,1 +0,0 @@
-A Minecraft 1.10.2 mod that adds various items and machines related to industrial processing chains
--- src/main/java/Superfrogman98/NCS/NexCorpSolutions.java
+++ src/main/java/Superfrogman98/NCS/NexCorpSolutions.java
@@ -6,25 +6,12 @@ package Superfrogman98.NCS;
  */
 
 //imports
-import Superfrogman98.NCS.blocks.ModBlocks;
-import Superfrogman98.NCS.blocks.ModBlocksCrops;
-import Superfrogman98.NCS.client.NCS_Tab;
-import Superfrogman98.NCS.items.ModItems;
-import Superfrogman98.NCS.items.tools.CustomToolClasses;
-import Superfrogman98.NCS.network.PacketRequestUpdateBasicWorktable;
-import Superfrogman98.NCS.network.PacketUpdateBasicWorktable;
-import Superfrogman98.NCS.recipes.ModRecipes;
-import Superfrogman98.NCS.sounds.ModSounds;
-import Superfrogman98.NCS.world.ModWorldGen;
+
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = NexCorpSolutions.modId, name = NexCorpSolutions.name, version = NexCorpSolutions.version, acceptedMinecraftVersions = "[1.10.2]")
 
@@ -32,21 +19,20 @@ public class NexCorpSolutions {
 
     public static final String modId = "nex_corp_solutions";
     public static final String name = "Nex Corp Solutions";
-    public static final String version = "0.0.1";
-    public static final NCS_Tab creativeTab = new NCS_Tab();
+    public static final String version = "0.0.1-mc1.12.2";
+    //public static final NCS_Tab creativeTab = new NCS_Tab();
     public static SimpleNetworkWrapper network;
 
-    //initialize proxy
-    @SidedProxy(serverSide = "Superfrogman98.NCS.ServerProxy", clientSide = "Superfrogman98.NCS.client.ClientProxy")
-    public static CommonProxy proxy;
+
 
     @Mod.Instance(modId)
     public static NexCorpSolutions instance;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event ) {
+
         System.out.println(name + " is loading!");
-        proxy.preInit(event);
+        /*proxy.preInit(event);
         ModSounds.init();
         ModBlocksCrops.init();
         ModItems.init();
@@ -55,19 +41,24 @@ public class NexCorpSolutions {
         network = NetworkRegistry.INSTANCE.newSimpleChannel(modId);
         network.registerMessage(new PacketUpdateBasicWorktable.Handler(), PacketUpdateBasicWorktable.class, 0 , Side.CLIENT );
         network.registerMessage(new PacketRequestUpdateBasicWorktable.Handler(), PacketRequestUpdateBasicWorktable.class, 1, Side.SERVER);
+        */
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
+        /*
         proxy.init(event);
 
         ModRecipes.init();
+        */
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
+        /*
         proxy.postInit(event);
         CustomToolClasses.init();
+        */
     }
 
 
